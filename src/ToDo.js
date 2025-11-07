c08640905627be7d4e70fccbe7c81c5695ac5e8e
Bhushan Kalode <bhushan.kalode@inkind.com>
2025-11-07 20:16:57 +0530

    Created ToDo component with basic buttons

---
+++ src/ToDo.js
@@ -0,0 +1,18 @@
+function ToDo() {
+  function addItem() {
+
+  }
+
+  function deleteItem() {
+    
+  }
+
+  return (
+    <div>
+      <button onClick={deleteItem}>Delete</button>
+      <button onClick={addItem}>Add</button>
+    </div>
+  );
+}
+
+export default ToDo;
